1d179908451a8c2030741b04f5f0d28e0fde4a7f
Michael Green <MichaelGreenBSEE@gmail.com>
2017-09-13 10:57:35 -0500

    Avoid hitting the car in front of us.

--- src/main.cpp
+++ src/main.cpp
@@ -257,9 +257,50 @@ int main() {
 
             int prev_size = previous_path_x.size();
 
+            //beginning of commented-out code
+            if(prev_size > 0)
+            {
+              car_s = end_path_s;
+            }
+
+            bool too_close = false;
+
+            //find ref_v to use
+            for(int i =0; i < sensor_fusion.size(); i++)
+            {
+              //car is in my lane
+              float d = sensor_fusion[i][6];
+              if(d < (2+4*lane+2) && d > (2+4*lane-2))
+              {
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx+vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+
+                check_car_s += ((double)prev_size*.02*check_speed); //if using previous points can projet s value out
+                //check s values greater than mine and s gap
+                if((check_car_s > car_s) && ((check_car_s-car_s) < 30))
+                {
+                  // Do some logic here, lower reference velocity so we don't crash into the car in fron of us, could
+                  // else flag to try to change lanes
+                  ref_vel = 29.5; //mph
+                  //too_close = true;
+                }
+
+
+              }
+            }
 
 
 
+
+
+
+
+
+
+
+            //end of commented-out code
 
             // Create a list of widely spaced (x,y) waypoints, evenly spaced at 30m
             // Later we will interpolate these waypoints with a spline and fill it in with more points that control speed
